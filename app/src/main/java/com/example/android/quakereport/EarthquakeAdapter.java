3b8b8e84b7e88aa2cb153b61616b2596294a4ea1
Fahri MacBook <fahri-can@web.de>
2017-10-17 00:25:22 +0200

    started with the custom adapter

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static android.R.attr.resource;
+
+/**
+ * Created by fahri on 17.10.17.
+ */
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+/*
+    public EarthquakeAdapter(Activity context, ArrayList<String> earthquakes) {
+        super();
+    }*/
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.support_simple_spinner_dropdown_item, parent, false);
+        }
+
+        Earthquake eq = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(eq.getMagnitude());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        if (currentWord.hasImage()) {
+
+            imageView.setImageResource(currentWord.getTmageRescourceId());
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else {
+            imageView.setVisibility(View.GONE);
+        }
+
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), colorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout
+        // so that it can be shown in the ListView
+        return listItemView;
+
+    }
